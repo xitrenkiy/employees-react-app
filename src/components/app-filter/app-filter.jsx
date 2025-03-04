94eacb98438fb2e362dd32ea981434091af5c7d0
Andrey <jscript1098@gmail.com>
2025-03-04 19:17:06 +0200

    Сделал уже очень много, реакт оказывается интересная вещь, сделал свою реализацию фильтров, а в следующем коммите добавлю адекватную фильтрацию

--- src/components/app-filter/app-filter.jsx
+++ src/components/app-filter/app-filter.jsx
@@ -1,25 +1,51 @@
+import { Component } from 'react';
+
 import './app-filter.css';
 
-const AppFilter = () => {
-	return (
-		<div className="btn-group">
-			<button 
-			className="btn btn-light"
-			type="button">
-				Все сотрудники
-			</button>
-			<button 
-			className="btn btn-outline-light"
-			type="button">
-				Сотрудники на повышение
-			</button>
-			<button 
-			className="btn btn-outline-light"
-			type="button">
-				З/П больше 1000$
-			</button>
-		</div>
-	)
+class AppFilter extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			btn: 'all'
+		}
+	}
+
+	onBtnClick = (e) => {
+		const target = e.currentTarget.getAttribute('data-btn');
+
+		this.setState({
+			btn: target
+		})
+		this.props.onFilterClick(target);
+	}
+
+	render() {
+		return (
+			<div className="btn-group">
+				<button 
+				className="btn btn-light"
+				type="button"
+				data-btn='all'
+				onClick={this.onBtnClick}>
+					Все сотрудники
+				</button>
+				<button 
+				className="btn btn-outline-light"
+				type="button"
+				data-btn='increase'
+				onClick={this.onBtnClick}>
+					Сотрудники на повышение
+				</button>
+				<button 
+				className="btn btn-outline-light"
+				type="button"
+				data-btn='major'
+				onClick={this.onBtnClick}>
+					З/П больше 1000$
+				</button>
+			</div>
+		)
+	}
 }
 
 export default AppFilter;
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -7,27 +9,113 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 
-function App() {
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{name: 'John W.', salary: 500, increase: true, rise: true, id: 1},
+				{name: 'Johnatan S.', salary: 1000, increase: true, rise: false, id: 2},
+				{name: 'William G.', salary: 5000, increase: false, rise: false, id: 3}
+			],
+			term: '',
+			btn: 'all'
+		}
+		this.maxId = 4;
+	}
 
-	const data = [
-		{name: 'John W.', salary: 500, increase: false, id: 1},
-		{name: 'Johnatan S.', salary: 1000, increase: false, id: 2},
-		{name: 'William G.', salary: 5000, increase: false, id: 3}
-	];
+	deleteItem = (id) => {
+		this.setState(({data}) => {
+			return {
+				data: data.filter(item => item.id !== id)
+			}
+		})
+	}
 
-	return (
-		<div className="app">
-			<AppInfo />
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+			rise: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
 
-			<div className="search-panel">
-				<SearchPanel />
-				<AppFilter />
+	onToggleProp = (id, prop) => {
+		this.setState(({data}) => ({
+			data: data.map(item => {
+				if (item.id === id) {
+					return {...item, [prop]: !item[prop]}
+				}
+				return item;
+			})
+		}))
+	}
+
+	searchEmp = (items, term) => {
+		if (term.length === 0) {
+			return items;
+		}
+
+		return items.filter(item => {
+			return item.name.indexOf(term) > -1
+		})
+	}
+
+	onUpdateSearch = (term) => {
+		this.setState({term});
+	}
+
+	onBtnClick = (items) => {
+		return items.filter(item => {
+			return item.rise === true;
+		});
+	}
+
+	majorEmployee = (items) => {
+		return items.filter(items => items.salary > 999);
+	}
+
+	onFilterClick = (btn) => {
+		this.setState({btn});
+	}
+
+	render() {
+		const { data, term, btn } = this.state;
+		const employees = data.length;
+		const increased = data.filter(item => item.increase).length;
+		const visibleData = this.searchEmp(data, term);
+		const riseFilter = this.onBtnClick(data);
+		const majorFilter = this.majorEmployee(data);
+
+		return (
+			<div className="app">
+				<AppInfo employees={employees}
+						 increased={increased}
+				/>
+	
+				<div className="search-panel">
+					<SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+					<AppFilter onFilterClick={this.onFilterClick}/>
+				</div>
+	
+				<EmployeesList 
+						data={btn === 'increase' ? riseFilter : btn === 'major' ? majorFilter : visibleData}
+						onDelete={this.deleteItem}
+						onToggleProp={this.onToggleProp}
+						/>
+				<EmployeesAddForm 
+						onAdd={this.addItem}/>
 			</div>
-
-			<EmployeesList data={data}/>
-			<EmployeesAddForm />
-		</div>
-	)
+		)
+	}
 }
 
 export default App;
